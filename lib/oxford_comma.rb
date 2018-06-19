f9c9623d3259adbd67adbd1917fccd78616dcf35
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-19 04:13:01 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
             when 2
                   array.join(" and ")
             when 3
-                  array.[array.size - 1] = "and #{array.last}"
+                  array[array.size - 1] = "and #{array.last}"
                   array.join(", ")
             else
                   array.join(", ")
