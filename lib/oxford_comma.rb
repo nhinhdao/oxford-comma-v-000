3251d693c83ddf017451ba30bf65b402714e1b47
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-19 03:51:47 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+      case array.size do
+            when 2
+                  array.join(" and ")
+            when 3
+                  last = array.size - 1
+                  array.pop
+                  array.join(", ").push(last)
+                  array
+            else
+                  array.join(", ") 
+      end
+
+
 
 end
